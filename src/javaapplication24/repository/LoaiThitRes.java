73fa7a9df21d481efee718a4967af3a8926da148
DuongThanhGiang <128674680+DuongThanhGiang@users.noreply.github.com>
2023-04-08 07:44:35 +0700

    Add files via upload

---
+++ src/javaapplication24/repository/LoaiThitRes.java
@@ -0,0 +1,135 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Responsitory;
+
+import DomainModel.LoaiThit;
+import DomainModel.Thit;
+import Utilities.DBContext;
+import Utilities.JDBCHelpers;
+import java.util.*;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Admin
+ */
+public class LoaiThitRes implements InLoaiThitRes{
+    
+    @Override
+    public ArrayList<LoaiThit> read(){
+        ArrayList<LoaiThit> listLT = new ArrayList<>();
+        String sql = "SELECT LOAITHIT.ID, LOAITHIT.TEN, LOAITHIT.GIATHEOKG, LOAITHIT.HSD, THIT.ID, THIT.TEN, THIT.MOTA FROM LOAITHIT JOIN THIT ON THIT.ID = LOAITHIT.IDTHIT ORDER BY LOAITHIT.ID";
+        ResultSet rs = JDBCHelpers.executeQuery(sql);
+        try {
+            while (rs.next()) {
+                String Id = rs.getString(1);
+                String Ten = rs.getString(2);
+                int DonGia = Integer.parseInt(rs.getString(3));
+                int HSD = Integer.parseInt(rs.getString(4));
+                listLT.add(new LoaiThit(Id, rs.getString(5), Ten, DonGia, HSD));
+            }
+            return listLT;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoaiThitRes.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    
+    @Override
+    public LoaiThit getLTByMa(String Ma){
+        LoaiThit lt = null;
+        String sql = "SELECT LOAITHIT.ID, LOAITHIT.TEN, LOAITHIT.GIATHEOKG, LOAITHIT.HSD, THIT.ID, THIT.TEN, THIT.MOTA FROM LOAITHIT JOIN THIT ON THIT.ID = LOAITHIT.IDTHIT WHERE LOAITHIT.ID = ? ORDER BY LOAITHIT.ID";
+        ResultSet rs = JDBCHelpers.executeQuery(sql,Ma);
+        try {
+            while (rs.next()) {
+                String Id = rs.getString(1);
+                String Ten = rs.getString(2);
+                int DonGia = Integer.parseInt(rs.getString(3));
+                int HSD = Integer.parseInt(rs.getString(4));
+                lt = new LoaiThit(Id, rs.getString(5), Ten, DonGia, HSD);
+            }
+            return lt;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoaiThitRes.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    
+    @Override
+    public LoaiThit getLTByTen(String TenLT){
+        LoaiThit lt = null;
+        String sql = "SELECT LOAITHIT.ID, LOAITHIT.TEN, LOAITHIT.GIATHEOKG, LOAITHIT.HSD, THIT.ID, THIT.TEN, THIT.MOTA FROM LOAITHIT JOIN THIT ON THIT.ID = LOAITHIT.IDTHIT WHERE LOAITHIT.TEN = ? ORDER BY LOAITHIT.ID";
+        ResultSet rs = JDBCHelpers.executeQuery(sql,TenLT);
+        try {
+            while (rs.next()) {
+                String Id = rs.getString(1);
+                String Ten = rs.getString(2);
+                int DonGia = Integer.parseInt(rs.getString(3));
+                int HSD = Integer.parseInt(rs.getString(4));
+                lt = new LoaiThit(Id, rs.getString(5), Ten, DonGia, HSD);
+            }
+            return lt;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoaiThitRes.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    
+    @Override
+    public LoaiThit getLTByGia(String Gia){
+        LoaiThit lt = null;
+        String sql = "SELECT LOAITHIT.ID, LOAITHIT.TEN, LOAITHIT.GIATHEOKG, LOAITHIT.HSD, THIT.ID, THIT.TEN, THIT.MOTA FROM LOAITHIT JOIN THIT ON THIT.ID = LOAITHIT.IDTHIT WHERE LOAITHIT.GIATHEOKG = ? ORDER BY LOAITHIT.ID";
+        ResultSet rs = JDBCHelpers.executeQuery(sql,Gia);
+        try {
+            while (rs.next()) {
+                String Id = rs.getString(1);
+                String Ten = rs.getString(2);
+                int DonGia = Integer.parseInt(rs.getString(3));
+                int HSD = Integer.parseInt(rs.getString(4));
+                lt = new LoaiThit(Id, rs.getString(5), Ten, DonGia, HSD);
+            }
+            return lt;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoaiThitRes.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    
+    @Override
+    public int add(LoaiThit lt){
+        String sql = "INSERT INTO LOAITHIT (IDTHIT, TEN, GIATHEOKG, HSD) VALUES (?,?,?,?)";
+        return JDBCHelpers.executeUpdate(sql, lt.getIDThit(), lt.getTen(), lt.getDonGia(), lt.getHSD());
+    }
+    
+    
+    @Override
+    public int delete(String Ma){
+        String sql = "DELETE FROM LOAITHIT WHERE ID = ?";
+        return JDBCHelpers.executeUpdate(sql, Ma);
+    }
+    
+    
+    @Override
+    public int deleteByThit(String Ma){
+        String sql = "DELETE FROM LOAITHIT WHERE IDTHIT = ?";
+        return JDBCHelpers.executeUpdate(sql, Ma);
+    }
+    
+    @Override
+    public int update(LoaiThit lt){
+        String sql = "UPDATE LOAITHIT SET IDTHIT = ?, TEN = ?, GIATHEOKG = ?, HSD = ? WHERE ID = ?";
+        return JDBCHelpers.executeUpdate(sql, lt.getIDThit(), lt.getTen(), lt.getDonGia(), lt.getHSD(), lt.getId());
+    }
+}
---
+++ src/javaapplication24/repository/ThitRes.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Responsitory;
+
+import DomainModel.Thit;
+import Utilities.DBContext;
+import Utilities.JDBCHelpers;
+import java.util.*;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Admin
+ */
+public class ThitRes implements InThitRes{
+    @Override
+    public ArrayList<Thit> read(){
+        ArrayList<Thit> listT = new ArrayList<>();
+        try {
+            Connection connection = DBContext.getConnection();
+            String sql = "SELECT * FROM THIT";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                listT.add(new Thit(rs.getString(1), rs.getString(2), rs.getString(3)));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Loi ket noi !!!");
+            Logger.getLogger(ThitRes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listT;
+    }
+    
+    public Thit getThitByMa(String Ma){
+        try {
+            Connection connection = DBContext.getConnection();
+            String sql = "SELECT * FROM THIT WHERE ID = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, Ma);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                return new Thit(rs.getString(1), rs.getString(2), rs.getString(3));
+            }
+            connection.close();
+            rs.close();
+            ps.close();
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public Thit getThitByTen(String Ten){
+        try {
+            Connection connection = DBContext.getConnection();
+            String sql = "SELECT * FROM THIT WHERE TEN = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, Ten);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                return new Thit(rs.getString(1), rs.getString(2), rs.getString(3));
+            }
+            connection.close();
+            rs.close();
+            ps.close();
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public int add(Thit t){
+        String sql = "INSERT INTO THIT (TEN, MOTA) VALUES (?, ?)";
+        return JDBCHelpers.executeUpdate(sql, t.getTen(), t.getMota());
+    }
+    
+    
+    public int delete(String Ma){
+        String sql = "DELETE FROM THIT WHERE ID = ?";
+        return JDBCHelpers.executeUpdate(sql, Ma);
+    }
+    
+    public int update(Thit t){
+        String sql = "UPDATE THIT SET TEN = ?, MOTA = ? WHERE ID = ?";
+        return JDBCHelpers.executeUpdate(sql, t.getTen(), t.getMota(), t.getId());
+    }
+}
